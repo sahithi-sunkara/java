96305e33558324f66e1e2c6831b84d2e2f2db9a1
sahithi-sunkara <151710691+sahithi-sunkara@users.noreply.github.com>
2024-10-17 11:46:02 +0530

    Create Point3D.java

---
+++ Point3D.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class Point2D{
+    int x,y;
+    void display(){
+        int x=5,y=10;
+        System.out.println("The coordinates are " +x+"," +y);
+    }
+}
+class Point3D extends Point2D{
+    int z=15;
+    void display(){
+        System.out.println("The coordinates are " +x+ "," +y+ "," +z);
+    }
+public static void main(String args[]){
+    Point2D ob1 =new Point2D();
+    Point3D ob2 =new Point3D();
+    Point2D ob3 = new Point3D();
+    ob1.display();
+    ob2.display();
+    ob3.display();
+  }
+}
