7fe07e106a8e063a9f985c7dc6bdc0d94ba54f6f
sahithi-sunkara <151710691+sahithi-sunkara@users.noreply.github.com>
2024-10-17 20:55:48 +0530

    Create WhoAmI.java

---
+++ WhoAmI.java
@@ -0,0 +1,21 @@
+import java.io.*;
+class A{
+    void CallMe(){
+        System.out.println("I am from A");
+    }
+}
+class B extends A{
+    void CallMe(){
+        System.out.println("I am from B");
+    }
+}
+class WhoAmI{
+    public static void main(String args[]){
+        A a1=new A();
+        A a2=new B();
+        B b=new B();
+        a1.CallMe();
+        a2.CallMe();
+        b.CallMe();
+    }
+}
