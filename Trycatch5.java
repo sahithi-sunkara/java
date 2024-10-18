e4c783b6174ce80aaaf802a55ce305e86538eb70
sahithi-sunkara <151710691+sahithi-sunkara@users.noreply.github.com>
2024-10-18 20:14:19 +0530

    Create Trycatch5.java

---
+++ Trycatch5.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+class Trycatch5
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			throw new IOException();
+		}
+		
+		catch(IOException ioe)
+		{
+			System.out.println("IOException caught "+ioe);
+		}
+		
+	}
+}
