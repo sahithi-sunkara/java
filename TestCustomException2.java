5ea931a6a94eeb5fc15079e8c70f17f21df19190
sahithi-sunkara <151710691+sahithi-sunkara@users.noreply.github.com>
2024-10-18 21:01:53 +0530

    Create TestCustomException2.java

---
+++ TestCustomException2.java
@@ -0,0 +1,35 @@
+class invalidAgeException extends Exception
+{
+	public invalidAgeException(String str)
+	{
+		super(str);
+	}
+}
+
+ public class TestCustomException2
+{
+	static void validate(int age) throws invalidAgeException
+	{
+		if(age<18)
+		{
+			throw new invalidAgeException("age is not valid to vote");
+		}
+		else
+		{
+			System.out.println("Welcome to vote");
+		}
+	}
+	public static void main(String args[])
+	{
+		try
+		{
+			validate(13);
+		}
+		catch(invalidAgeException ex)
+		{
+			System.out.println("caught throw exception");
+			System.out.println("Exception occured : "+ex);
+		}
+		System.out.println("rest of the code...");
+	}
+}
