a76a5ad140b0bb3f52da902220b7ad936441707e
sahithi-sunkara <151710691+sahithi-sunkara@users.noreply.github.com>
2024-10-18 18:28:46 +0530

    Create  FibRec.java

---
+++ FibRec.java
@@ -0,0 +1,27 @@
+import java.io.*;
+class FibRec{
+  int Fib(int n)
+  {
+    if(n==0)
+    {
+      return 0;
+    }
+    else if(n==1)
+    {
+      return 1;
+    }
+else
+    {
+      return Fib(n-1)+Fib(n-2));
+    }
+  }
+  public static void main(String args[])
+  {
+    FibRec ob = new FibRec();
+    int n = Integer.parseInt(args[0]);
+    for(int i=0;i<n;i++)
+    {
+      System.out.println(ob.Fib(i));
+    }
+  }
+}
