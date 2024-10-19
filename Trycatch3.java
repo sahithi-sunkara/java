b375e5d2744486a127e7c3591d53b0add669afd9
sahithi-sunkara <151710691+sahithi-sunkara@users.noreply.github.com>
2024-10-19 11:12:14 +0530

    Create Trycatch3.java

---
+++ Trycatch3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Trycatch3{
+    public static void main(String args[]){
+        int a=5,b=0,c;
+        try{
+            c=a/b;
+            System.out.println("The value of c:"+c);
+        }
+        catch(Exception e){
+            System.out.println("Exception caught: "+e);
+        }
+        catch(ArithmeticException ae){
+            System.out.println("ArithmeticException caught: "+ae);
+        }
+    }
+}
