c5f1d981f7c547fc1e7fd77280cbeee57a83af32
sahithi-sunkara <151710691+sahithi-sunkara@users.noreply.github.com>
2024-10-26 12:12:33 +0530

    Create printable.java

---
+++ printable.java
@@ -0,0 +1,13 @@
+import java.io.*;
+interface printable{
+    void print();
+}
+class A implements printable{
+    public void print(){
+        System.out.println("Hello");
+    }
+    public static void main(String args[]){
+        A obj=new A();
+        obj.print();
+    }
+}
