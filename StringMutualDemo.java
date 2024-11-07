a67d5824b65a6884395adcb6930df0d9ac4a8930
sahithi-sunkara <151710691+sahithi-sunkara@users.noreply.github.com>
2024-11-07 10:18:29 +0530

    Create StringMutualDemo.java

---
+++ StringMutualDemo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.lang.String.*;
+class StringMutualDemo{
+    public static void main(String args[]){
+        String s = "sasi";
+        s=s.concat("site");
+        System.out.println("After concating s1 and s2 "+s); 
+        String s2 = "sahithi";
+        s=s2.concat("sunkara");
+        System.out.println("After concating s1 and s2 "+s);
+    }
+}
